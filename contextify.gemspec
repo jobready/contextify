924886ee2abffeeb07aeafababd091f8c96c8ad0
Dan Draper <daniel@codefire.com>
2014-08-08 13:26:11 +1000

    Updated policy behaviour, added specs, updated README

--- contextify.gemspec
+++ contextify.gemspec
@@ -21,4 +21,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_dependency "activesupport"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec"
+  spec.add_development_dependency "rails", ">= 4.0"
 end
--- lib/contextify/permission.rb
+++ lib/contextify/permission.rb
@@ -1,26 +1,47 @@
 module Contextify::Permission
   extend ActiveSupport::Concern
-  # @return true if there is a Policy class available to complement the permission
-  def has_policy?
-    policy_class
-    true
-  rescue NameError
-    false
+
+  class Naming
+    def self.action_class_name(type, action)
+      [action.camelize, type, 'Policy'].join
+    end
+
+    def self.class_name(type)
+      [type, 'Policy'].join
+    end
   end
 
-  # The name of the policy class if one is present
+  # @return true if there is a Policy class specific to the action available to complement the permission
+  def has_action_policy?
+    self.class.const_defined?(Naming.action_class_name(object_type, action_name))
+  end
+
+  # @return true if there is a Policy class for the type available to complement the permission
+  def has_type_policy?
+    self.class.const_defined?(Naming.class_name(object_type))
+  end
+
+  # The Policy class for the specific action if one is present
   # @raise NameError if none present
-  def policy_class
-    [action_name.camelize, object_type, 'Policy'].join.constantize
+  def policy_action_class
+    Naming.action_class_name(object_type, action_name).constantize
+  end
+
+  # The Policy class for the type if one is present
+  # (Only used if there is no specific action class)
+  # @raise NameError if none present
+  def policy_type_class
+    Naming.class_name(object_type).constantize
   end
 
   # Apply this permission to the ability, directly via cancan
   # or via the Policy object if one is available
   #
   def apply_to(ability)
-    if has_policy?
-      policy = policy_class.new(ability)
-      policy.apply!
+    if has_action_policy?
+      policy_action_class.new(ability).apply!
+    elsif has_type_policy?
+      policy_type_class.new(ability).apply!
     else
       ability.can(action_name.to_sym, object_type.constantize)
     end
